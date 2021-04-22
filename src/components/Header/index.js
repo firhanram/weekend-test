c8160404e06036455ac92cd03bce0c9e62986c3c
Firhan Ramadhan <59257112+firhanram@users.noreply.github.com>
2021-04-22 23:49:43 +0700

    changing line-height

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,8 +6,8 @@ const Header = () => {
             <div className="flex items-center w-5/6 lg:w-9/12 mx-auto">
                 <img src={Logo} alt="Logo" className="w-8 h-8"/>
                 <div className="ml-3">
-                    <h3 className="text-xs leading-tight">Good Morning</h3>
-                    <h3 className="font-black leading-tight">Fellas</h3>
+                    <h3 className="text-xs leading-none">Good Morning</h3>
+                    <h3 className="font-black">Fellas</h3>
                 </div>
             </div>
         </header>
