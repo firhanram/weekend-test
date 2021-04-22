a83466ec445662d22547b90389931923dd256ec6
Firhan Ramadhan <59257112+firhanram@users.noreply.github.com>
2021-04-23 00:47:19 +0700

    customing box-shadow and height

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,23 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    backgroundColor: theme => ({
+      ...theme('colors'),
+      'primary':'#eebece',
+      'secondary':'#0b24fb'
+    }),
+    boxShadow: {
+      'primary': '0 20px 30px 0 rgba(249, 131, 171, 0.5)'
+    },
     extend: {
       fontFamily: {
         'primary' : ['Work Sans']
       },
       height: {
-        hero: '41rem'
+        section: '41rem',
+      },
+      borderRadius: {
+        '4xl': '1.8rem'
       }
     },
   },
