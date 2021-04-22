ee8f11e92a72db2c71d8fba650dcea14a585cb1b
Firhan Ramadhan <59257112+firhanram@users.noreply.github.com>
2021-04-23 01:22:12 +0700

    theming text color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
       'primary':'#eebece',
       'secondary':'#0b24fb'
     }),
+    textColor: theme => ({
+      'secondary':'#0b24fb',
+      'primary' : '#ffffff'
+    }),
     boxShadow: {
       'primary': '0 20px 30px 0 rgba(249, 131, 171, 0.5)'
     },
