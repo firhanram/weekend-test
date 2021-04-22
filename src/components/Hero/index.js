6135a1135ebed24238fbc58c1ebaf20062c44d83
Firhan Ramadhan <59257112+firhanram@users.noreply.github.com>
2021-04-23 04:38:24 +0700

    fixing postion button

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -14,7 +14,7 @@ const Hero = () => {
                 <h3 className="text-primary font-semibold mt-5">Stay active with a little workout</h3>
                 <div className="flex mt-16 flex-col items-center relative">
                     <img src={Lego} alt="Lego" className="w-44"/>
-                    <button className="px-6 w-56 h-14 -translate-y-2/4 top-3/4 rounded-4xl bg-white shadow-primary absolute">Let's Go</button>
+                    <button className="px-6 w-56 h-14 -translate-y-2/4 top-2/3 rounded-4xl bg-white shadow-primary absolute">Let's Go</button>
                 </div>
             </div>
         </section>
