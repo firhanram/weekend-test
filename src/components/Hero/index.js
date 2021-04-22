a855c16f1018cd3d29eac080d201afe177fecf30
Firhan Ramadhan <59257112+firhanram@users.noreply.github.com>
2021-04-23 00:47:56 +0700

    styling hero section

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,6 +1,23 @@
+import Background from '../../assets/images/background.png'
+import Lego from '../../assets/images/lego.png'
+
+// Adding background to Hero section
+const style = {
+    backgroundImage: `url(${Background})`
+}
+
 const Hero = () => {
     return (
-        <div>Hero</div>
+        <section className="w-full pt-32 pb-5 h-section bg-primary bg-no-repeat bg-cover bg-center relative" style={style}>
+            <div className="flex flex-col w-full h-full items-center relative">
+                <h1 className="uppercase font-black text-5xl text-white text-center">weekend from home</h1>
+                <h3 className="text-white font-semibold mt-5">Stay active with a little workout</h3>
+                <div className="flex mt-16 flex-col items-center relative">
+                    <img src={Lego} alt="Lego" className="w-44"/>
+                    <button className="px-6 w-56 h-14 -translate-y-2/4 top-3/4 rounded-4xl bg-white shadow-primary absolute">Let's Go</button>
+                </div>
+            </div>
+        </section>
     )
 }
 
