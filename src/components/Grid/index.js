8332d0d8c648a8cd6fecb3089ffb6abbfa52e599
Firhan Ramadhan <59257112+firhanram@users.noreply.github.com>
2021-04-23 20:43:12 +0700

    adding padding right to grid content

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -4,8 +4,8 @@ const Grid = ({data}) => {
     const renderCards = () => {
         return data.map(({imageURL, text}) => (
             <div className="relative">
-                <div className="w-full bg-black bg-opacity-50  py-4 lg:py-2 px-6 lg:h-16 flex justify-between absolute bottom-0">
-                  <h3 className="font-bold text-primary">{text}</h3>
+                <div className="w-full bg-black bg-opacity-50 py-4 lg:py-2 px-6 lg:h-16 flex justify-between absolute bottom-0">
+                  <h3 className="font-bold text-primary xl:pr-20">{text}</h3>
                   <img src={NextIcon} alt="Next" className="w-8 h-8 ml-4"/>
                 </div>
                 <img src={imageURL} alt="Accusamus" className="w-full"/>
